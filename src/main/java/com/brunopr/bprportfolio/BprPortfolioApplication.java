c4f9b79b286d52e03d11a4bf430bd83c99884608
Bruno Ribeiro <brunopr.bpr@gmail.com>
2022-05-31 19:03:04 +0100

    First Commit
    Starting Spring Application

---
+++ src/main/java/com/brunopr/bprportfolio/BprPortfolioApplication.java
@@ -0,0 +1,13 @@
+package com.brunopr.bprportfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BprPortfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BprPortfolioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brunopr/bprportfolio/BprPortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brunopr.bprportfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BprPortfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

6795a2f159fd829bba653e6a306cc7aa3c71546a
Bruno Ribeiro <brunopr.bpr@gmail.com>
2022-06-01 23:14:58 +0100

    Enabled connection to MongoDB

--- src/main/java/com/brunopr/bprportfolio/BprPortfolioApplication.java
+++ src/main/java/com/brunopr/bprportfolio/BprPortfolioApplication.java
@@ -2,7 +2,10 @@ package com.brunopr.bprportfolio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class BprPortfolioApplication {
 
@@ -10,4 +13,8 @@ public class BprPortfolioApplication {
 		SpringApplication.run(BprPortfolioApplication.class, args);
 	}
 
+	@GetMapping()
+	public String HomePage(){
+		return "Hello World!";
+	}
 }
